0c21572795c440fa3b7550e271439ae4811644e0
moezabdelkef <moezbadelkefi17@gmail.com>
2023-09-02 15:54:36 +0100

    create a binary tree

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -10,19 +10,18 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node = malloc(sizeof(binary_tree_t));
-    /*checks if the memory allocation was successful*/
-    if (node)
-    {
-        /*assigns the integer value to the n member of
-        the newly created node pointed to by node*/
-        node->n = value;
-        /* sets the parent member of the newly created */
-        node->parent = parent;
-        /*initialize the left and right pointers of
-        the newly created node to NULL*/
-        node->left = NULL;
-        node->right = NULL;
-    }
-    return (node);
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	/*checks if the memory allocation was successful*/
+	if (node)
+	{
+		/*assigns the integer value to the n member of the newly*/
+		/*created node pointed to by node*/
+		node->n = value;
+		/* sets the parent member of the newly created */
+		node->parent = parent;
+		/*initialize the left and right pointers of the newly created node to NULL*/
+		node->left = NULL;
+		node->right = NULL;
+	}
+	return (node);
 }
