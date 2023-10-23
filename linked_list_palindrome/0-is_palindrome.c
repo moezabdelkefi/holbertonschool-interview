b00ec40856790cc4ab155b6be0bf5576601bc16a
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-23 17:52:32 +0100

    Linked list palindrome

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -11,14 +11,21 @@ int is_palindrome(listint_t **head)
     if (*head == NULL)
         return (1);
 
-    listint_t *current = *head;
+    listint_t *current;
     listint_t *array[4096];
+    int len = 0;
     int i, j;
 
-    for (i = 0, j = 0; current != NULL; i++, j++, current = current->next)
-        array[i] = current;
+    current = *head;
 
-    for (i = 0, j--; i < j; i++, j--)
+    while (current != NULL)
+    {
+        array[len] = current;
+        current = current->next;
+        len++;
+    }
+
+    for (i = 0, j = len - 1; i < j; i++, j--)
     {
         if (array[i]->n != array[j]->n)
             return (0);
