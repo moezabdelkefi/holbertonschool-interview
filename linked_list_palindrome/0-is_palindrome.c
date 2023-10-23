28eafd29e450a09dfdf630743533261ae093e837
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-23 17:46:19 +0100

    Linked list palindrome

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to a pointer to the head of the list
+ * Return: 1 if the list is a palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+    if (*head == NULL)
+        return (1);
+
+    int len = 0;
+    listint_t *current = *head;
+    listint_t *array[4096];
+
+    while (current != NULL)
+    {
+        array[len++] = current;
+        current = current->next;
+    }
+
+    int i, j;
+
+    for (i = 0, j = len - 1; i < j; i++, j--)
+    {
+        if (array[i]->n != array[j]->n)
+            return (0);
+    }
+
+    return (1);
+}
