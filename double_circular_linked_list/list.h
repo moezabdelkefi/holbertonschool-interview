2dc01a564626791785fe7e456721be9602242f53
moezabdelkef <moezbadelkefi17@gmail.com>
2024-03-08 23:25:22 +0100

    Add Nodes

--- double_circular_linked_list/list.h
+++ double_circular_linked_list/list.h
@@ -1,6 +1,15 @@
 #ifndef LIST_H
 #define LIST_H
 
+/**
+ * struct List - doubly linked list
+ * @str: string - (malloc'ed string)
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
 typedef struct List
 {
     char *str;
