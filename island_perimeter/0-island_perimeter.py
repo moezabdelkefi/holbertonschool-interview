7623dbccf29b9e76d2fb2e58db5851f970140f64
moezabdelkef <moezbadelkefi17@gmail.com>
2024-03-28 04:03:29 +0100

    Island Perimeter

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" This function calculates the perimeter of an island represented in a 2D grid. """
+
+
+def island_perimeter(grid):
+    if not grid:
+        return 0
+
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
