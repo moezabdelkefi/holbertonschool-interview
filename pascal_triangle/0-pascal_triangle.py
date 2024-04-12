a30302bc5076854e586905c8b9a5e0958fe374e6
moezabdelkef <moezbadelkefi17@gmail.com>
2024-04-12 23:23:54 +0100

    Returns a list of lists of integers representing     the Pascal’s triangle of n

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -6,7 +6,7 @@ Pascal's Triangle
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing 
+    Returns a list of lists of integers representing
     the Pascal’s triangle of n
     """
     if n <= 0:
