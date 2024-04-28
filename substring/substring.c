8f58fd5d453045a84b5b7e017611de0c540d0224
moez <moezabdelkefi17@gmail.com>
2024-04-28 15:37:37 +0200

    Substring with concatenated words

--- substring/substring.c
+++ substring/substring.c
@@ -1,44 +1,49 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
-int *find_substring(char const *s, char const **words, int nb_words, int *n) {
-    int len, slen, i, j, k, found, start;
-    int *result, *map, *tmp;
+int is_valid_substr(char const *s, char const **words, int nb_words, int word_len, int start) {
+    int *seen = calloc(nb_words, sizeof(int));
+    int words_found = 0;
 
-    *n = 0;
-    len = strlen(words[0]);
-    slen = strlen(s);
-    result = malloc(slen * sizeof(int));
-    map = malloc(nb_words * sizeof(int));
-    tmp = malloc(nb_words * sizeof(int));
+    for (int i = 0; i < nb_words; i++) {
+        char *word = (char *)malloc((word_len + 1) * sizeof(char));
+        strncpy(word, s + start + i * word_len, word_len);
+        word[word_len] = '\0';
 
-    for (i = 0; i < nb_words; i++) {
-        for (j = i + 1; j < nb_words; j++) {
-            if (strcmp(words[i], words[j]) == 0)
-                map[i]++;
-        }
-    }
-
-    for (i = 0; i <= slen - nb_words * len; i++) {
-        memcpy(tmp, map, nb_words * sizeof(int));
-        for (j = 0; j < nb_words; j++) {
-            found = 0;
-            for (k = 0; k < nb_words; k++) {
-                start = i + j * len;
-                if (tmp[k] > 0 && strncmp(s + start, words[k], len) == 0) {
-                    found = 1;
-                    tmp[k]--;
-                    break;
-                }
-            }
-            if (found == 0)
+        for (int j = 0; j < nb_words; j++) {
+            if (!seen[j] && strcmp(word, words[j]) == 0) {
+                seen[j] = 1;
+                words_found++;
                 break;
+            }
         }
-        if (j == nb_words)
-            result[(*n)++] = i;
+        free(word);
     }
 
-    free(map);
-    free(tmp);
-    return result;
+    free(seen);
+    return words_found == nb_words;
+}
+
+int *find_substring(char const *s, char const **words, int nb_words, int *n) {
+    if (!s || !words || nb_words <= 0) {
+        *n = 0;
+        return NULL;
+    }
+
+    int word_len = strlen(words[0]);
+    int str_len = strlen(s);
+    int substr_count = 0;
+    int *indices = NULL;
+
+    for (int i = 0; i <= str_len - nb_words * word_len; i++) {
+        if (is_valid_substr(s, words, nb_words, word_len, i)) {
+            substr_count++;
+            indices = realloc(indices, substr_count * sizeof(int));
+            indices[substr_count - 1] = i;
+        }
+    }
+
+    *n = substr_count;
+    return indices;
 }
