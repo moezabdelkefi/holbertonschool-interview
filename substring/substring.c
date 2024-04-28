69b9d2968bd739660558fdb9c404b8276981319e
moez <moezabdelkefi17@gmail.com>
2024-04-28 15:41:43 +0200

    Substring with concatenated words

--- substring/substring.c
+++ substring/substring.c
@@ -5,13 +5,15 @@
 int is_valid_substr(char const *s, char const **words, int nb_words, int word_len, int start) {
     int *seen = calloc(nb_words, sizeof(int));
     int words_found = 0;
+    int i, j;
+    char *word;
 
-    for (int i = 0; i < nb_words; i++) {
-        char *word = (char *)malloc((word_len + 1) * sizeof(char));
+    for (i = 0; i < nb_words; i++) {
+        word = (char *)malloc((word_len + 1) * sizeof(char));
         strncpy(word, s + start + i * word_len, word_len);
         word[word_len] = '\0';
 
-        for (int j = 0; j < nb_words; j++) {
+        for (j = 0; j < nb_words; j++) {
             if (!seen[j] && strcmp(word, words[j]) == 0) {
                 seen[j] = 1;
                 words_found++;
