a3a717c845ff7ecbaf35a140a727bfb1e3bafb0d
moez <moezabdelkefi17@gmail.com>
2024-05-27 03:45:14 -1200

    Change comes from within

---
+++ making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
